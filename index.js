bf68d8723a80033becfda136f4b1b435c797ac61
David's Devel <djgm1206@gmail.com>
2023-01-19 22:05:26 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+
+  const {ip, headers} = req;
+  const software = headers['user-agent'];
+  const language = headers['accept-language'];
+
+  res.json({ip, language, software});
 });
 
 // listen for requests :)
